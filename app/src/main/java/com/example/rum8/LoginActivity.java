80a28dfb73f0c39da963c247ca6eb5891e81f591
Hao <hal384@ucsd.edu>
2019-04-23 22:48:12 -0700

    Frame work for log in and password recovery

--- app/src/main/java/com/example/rum8/LoginActivity.java
+++ app/src/main/java/com/example/rum8/LoginActivity.java
@@ -1,13 +1,106 @@
 package com.example.rum8;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.quickstart.auth.R;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //member variables for text field
+    private EditText emailField;
+    private EditText passwordField;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // views
+        emailField = findViewById(R.id.input_email);
+        passwordField = findViewById(R.id.input_password);
+
+        //buttons
+        //TODO: should be forget password bottom
+        findViewById(R.id.button_register).setOnClickListener(this);
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+    }
+
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //TODO: update UI based on user status
+    }
+    // [END on_start_check_user]
+
+    private void signIn(String email, String password) {
+        //TODO: get rid of compile error (might relate to import or package)
+/*        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            mStatusTextView.setText(R.string.auth_failed);
+                        }
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+        */
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        //TODO: [front end] should be log in button
+        if (i == R.id.button_register){
+            signIn(emailField.getText().toString(), passwordField.getText().toString());
+        }
     }
 }
