6d01a72e9730256aaab06c105473c6704f518f02
Chase Sriprajittichai <sriprajittichai@gmail.com>
2019-04-27 01:43:10 -0700

    Create ProfileSettingsActivity and  button in MainActivity that goes to ProfileSettingsActivity

---
+++ app/src/main/java/com/example/rum8/activities/ProfileSettingsActivity.java
@@ -0,0 +1,29 @@
+package com.example.rum8.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rum8.R;
+import com.example.rum8.controllers.ProfileSettingsController;
+import com.example.rum8.listeners.ProfileSettingsControllerListener;
+
+public class ProfileSettingsActivity extends AppCompatActivity
+                                     implements ProfileSettingsControllerListener {
+
+    private ProfileSettingsController controller;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+        initViews();
+        initController();
+    }
+
+    private void initViews() {
+    }
+
+    private void initController() {
+    }
+
+}
---
+++ app/src/main/java/com/example/rum8/controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.example.rum8.controllers;
+
+import com.example.rum8.listeners.MainControllerListener;
+
+public class MainController {
+
+    private MainControllerListener controllerListener;
+
+    public MainController(final MainControllerListener controllerListener) {
+        this.controllerListener = controllerListener;
+    }
+
+    public void onGoToProfileSettingsButtonClicked() {
+        controllerListener.goToProfileSettings();
+    }
+
+}
