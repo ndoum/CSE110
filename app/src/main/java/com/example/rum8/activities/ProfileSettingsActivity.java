d26f70609234de4e017d35a318369ad648092949
aiiitingx <aihsieh@ucsd.edu>
2019-05-02 19:25:35 -0700

    button to upload profile pic

--- app/src/main/java/com/example/rum8/activities/ProfileSettingsActivity.java
+++ app/src/main/java/com/example/rum8/activities/ProfileSettingsActivity.java
@@ -1,6 +1,15 @@
 package com.example.rum8.activities;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
@@ -16,6 +25,10 @@ public class ProfileSettingsActivity extends AppCompatActivity
     private ProfileSettingsController controller;
     private ViewPager viewPager;
 
+    private Button buttonUploadProfileImage;
+    private ImageView imageUserProfile;
+    private static int result_load_image = 1;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +42,27 @@ public class ProfileSettingsActivity extends AppCompatActivity
         viewPager = findViewById(R.id.profile_settings_view_pager);
         viewPager.setAdapter(new ProfileSettingsViewPagerAdapter(getSupportFragmentManager()));
 
+        buttonUploadProfileImage = (Button) findViewById(R.id.general_info_profile_image_upload_button);
+        /*buttonUploadProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGallary = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGallary, result_load_image);
+            }
+        });
+        imageUserProfile = (ImageView) findViewById(R.id.general_info_profile_image_view);*/
     }
 
-    private void initController() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == result_load_image && resultCode == RESULT_OK && data != null){
+            Uri selectImage = data.getData();
+            imageUserProfile.setImageURI(selectImage);
+        }
+    }
+
+    private void initController(){
         controller = new ProfileSettingsController(this);
     }
 
