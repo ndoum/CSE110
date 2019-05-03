3c47fecc733a7c55382aabd9dafe0014e432b515
CassieYud <duy003@ucsd.edu>
2019-05-03 02:21:22 -0700

    Revert "indent"
    This reverts commit 68ff64a0ae9110d52d0765d712c76292afb671f2, reversing
    changes made to b75b84c1b734b887b165143292075f858c0c9b6c.

--- app/src/main/java/com/example/rum8/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/rum8/activities/RegistrationActivity.java
@@ -18,7 +18,6 @@ import com.example.rum8.listeners.RegistrationControllerListener;
 public class RegistrationActivity extends AppCompatActivity
 	implements RegistrationControllerListener {
 
-<<<<<<<HEAD
   private RegistrationController controller;
   private TextInputEditText editText_email, editText_password;
 
@@ -84,72 +83,5 @@ public class RegistrationActivity extends AppCompatActivity
   private void initController() {
 	controller = new RegistrationController(this, this);
   }
-=======
-  private RegistrationController controller;
-  private TextInputEditText editText_email, editText_password;
-
-  @Override
-  protected void onCreate(final Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_registration);
-	initViews();
-	initController();
-  }
-
-  @Override
-  protected void onStop() {
-	super.onStop();
-	controller.destroy();
-  }
-
-  @Override
-  public void onUserRegistered() {
-	final Intent intent = new Intent(RegistrationActivity.this, ProfileSettingsActivity.class);
-	startActivity(intent);
-	finish();
-  }
-
-  @Override
-  public void showToast(final String message, final int toastLength) {
-	Toast.makeText(RegistrationActivity.this, message, toastLength).show();
-  }
-
-  @Override
-  public void goBackToLogin() {
-	final Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
-	startActivity(intent);
-	finish();
-
-  }
-
-
-  private void initViews() {
-	editText_email = (TextInputEditText) findViewById(R.id.input_email);
-	editText_password = (TextInputEditText) findViewById(R.id.input_password);
-	final Button button_register = (Button) findViewById(R.id.button_register);
-
-	button_register.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(final View v) {
-		final String email = editText_email.getText().toString();
-		final String password = editText_password.getText().toString();
-		controller.onSubmit(email, password);
-	  }
-	});
-
-
-	final Button button_cancel = (Button) findViewById(R.id.button_registration_cancel);
-	button_cancel.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		controller.onGoBackToLoginButtonClicked();
-	  }
-	});
-  }
-
-  private void initController() {
-	controller = new RegistrationController(this, this);
-  }
->>>>>>>ccee4b2efd9085183cd9135317f7c4043493b2dc
 
 }
