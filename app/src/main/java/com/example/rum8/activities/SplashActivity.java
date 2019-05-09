6164caff90d01895adde1bc9b2b751eb3c9c372a
Chase Sriprajittichai <sriprajittichai@gmail.com>
2019-05-09 11:04:35 -0700

    Move conditional app launch behavior (go to next activity) from main controller to splash controller

--- app/src/main/java/com/example/rum8/activities/SplashActivity.java
+++ app/src/main/java/com/example/rum8/activities/SplashActivity.java
@@ -8,9 +8,12 @@ import android.view.Window;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rum8.R;
+import com.example.rum8.controllers.SplashController;
 import com.example.rum8.listeners.SplashControllerListener;
 
 public class SplashActivity extends AppCompatActivity implements SplashControllerListener {
+
+    private SplashController controller;
 
     private final int SPLASH_DISPLAY_TIME_MS = 2000;
 
@@ -19,13 +22,30 @@ public class SplashActivity extends AppCompatActivity implements SplashControlle
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+        initController();
 
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                startActivity( new Intent(SplashActivity.this, MainActivity.class));
-                finish();
+                controller.goToNextPage();
             }
-        }, msDelay);
+        }, SPLASH_DISPLAY_TIME_MS);
     }
 
+    @Override
+    public void goToMainPage() {
+        final Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void goToLogin() {
+        final Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initController() {
+        controller = new SplashController(this);
+    }
 }
