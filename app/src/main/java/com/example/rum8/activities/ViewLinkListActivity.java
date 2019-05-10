6f3d08614cebbc1d82e5ecff0ae64b04d4e7bdd8
Dorian Maldonado <d1maldon.ucsd.edu>
2019-05-10 03:21:25 -0700

    Created files necessary for the link list

---
+++ app/src/main/java/com/example/rum8/activities/ViewLinkListActivity.java
@@ -0,0 +1,11 @@
+package com.example.rum8.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rum8.listeners.ViewLinkListControllerListener;
+
+
+public class ViewLinkListActivity extends AppCompatActivity
+        implements ViewLinkListControllerListener {
+
+}
