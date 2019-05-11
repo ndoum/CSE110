093101d0538f279a4aec8562f9da60c42233e258
Ethan Yuan <etyuan@ucsd.edu>
2019-05-10 17:57:35 -0700

    Solved compile error

--- app/src/main/java/com/example/rum8/controllers/ProfileSettingsController.java
+++ app/src/main/java/com/example/rum8/controllers/ProfileSettingsController.java
@@ -10,6 +10,7 @@ import com.example.rum8.database.Db;
 import com.example.rum8.listeners.ProfileSettingsControllerListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
