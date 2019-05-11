a75c3cd2b7548ac014115c5344ee286fbce42a25
Dorian Maldonado <d1maldon.ucsd.edu>
2019-05-11 14:24:45 -0700

    Implemented necessary classes, initial methods needed

--- app/src/main/java/com/example/rum8/activities/ViewLinkListActivity.java
+++ app/src/main/java/com/example/rum8/activities/ViewLinkListActivity.java
@@ -1,11 +1,30 @@
 package com.example.rum8.activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.rum8.R;
+import com.example.rum8.controllers.ViewLinkListController;
 import com.example.rum8.listeners.ViewLinkListControllerListener;
 
 
 public class ViewLinkListActivity extends AppCompatActivity
         implements ViewLinkListControllerListener {
 
+    private ViewLinkListController controller;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_link_list);
+        initViews();
+        initController();
+    }
+
+    private void initViews() {
+
+    }
+
+    private void initController() { controller = new ViewLinkListController(this);}
 }
--- app/src/main/java/com/example/rum8/controllers/ViewLinkListController.java
+++ app/src/main/java/com/example/rum8/controllers/ViewLinkListController.java
@@ -1,4 +1,13 @@
 package com.example.rum8.controllers;
 
+import com.example.rum8.listeners.ViewLinkListControllerListener;
+
 public class ViewLinkListController {
+
+    private ViewLinkListControllerListener controllerListener;
+
+    public ViewLinkListController(final ViewLinkListControllerListener controllerListener) {
+        this.controllerListener = controllerListener;
+    }
+
 }
