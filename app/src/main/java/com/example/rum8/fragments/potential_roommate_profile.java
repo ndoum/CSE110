3b2cde3abfb40398a2862ece3af7dfaf9ba3179d
Ethan Yuan <etyuan@ucsd.edu>
2019-05-17 17:43:49 -0700

    Initialization

---
+++ app/src/main/java/com/example/rum8/fragments/potential_roommate_profile.java
@@ -0,0 +1,25 @@
+package com.example.rum8.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rum8.R;
+
+
+public class potential_roommate_profile extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_potential_roommate_profile, container, false);
+    }
+
+}
