bdcc08ae6dca810cfb0320f21ead810f29bdbe32
Ethan Yuan <etyuan@ucsd.edu>
2019-05-27 13:13:04 -0700

    Update PotentialRoommateProfileDefault.java

--- app/src/main/java/com/example/rum8/fragments/PotentialRoommateProfileDefault.java
+++ app/src/main/java/com/example/rum8/fragments/PotentialRoommateProfileDefault.java
@@ -4,22 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.rum8.R;
 import com.example.rum8.adapters.PotentialRoommateProfileDefaultAdapter;
+import com.example.rum8.controllers.MainController;
+import com.example.rum8.database.Db;
+import com.example.rum8.listeners.MainControllerListener;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
-public class PotentialRoommateProfileDefault extends Fragment {
+import java.util.Map;
+
+public class PotentialRoommateProfileDefault extends Fragment implements MainControllerListener {
 
     private TabLayout tablayout;
     private AppBarLayout appBarLayout;
     private ViewPager viewPager;
-
+    private MainController controller;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,6 +35,7 @@ public class PotentialRoommateProfileDefault extends Fragment {
         }
 
         View rootView = inflater.inflate(R.layout.fragment_potential_roommate_profile_default, container, false);
+
 
         tablayout = rootView.findViewById(R.id.potential_roommate_profile_default_tablayout_id);
         appBarLayout = rootView.findViewById(R.id.potential_roommate_profile_default_appbarid);
@@ -42,8 +49,51 @@ public class PotentialRoommateProfileDefault extends Fragment {
         viewPager.setAdapter(adapter);
         tablayout.setupWithViewPager(viewPager);
 
+
+
         // Inflate the layout for this fragment
         return rootView;
     }
+
+    @Override
+    public void onViewCreated(View rootView, Bundle savedInstanceState) {
+        controller = new MainController(this);
+        controller.loadUserInfo();
+        rootView.findViewById();
+
+
+
+    }
+
+
+    @Override
+    public void showCurrentUserInfo(final Map<String, Object> data) {
+
+
+
+        onResume();
+    }
+
+    @Override
+    public void showToast(final String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void goToProfileSettings() {
+    }
+
+    @Override
+    public void goToLogin() {
+    }
+
+    @Override
+    public void goToAdvSettings() {
+    }
+
+
+
+
+
 
 }
