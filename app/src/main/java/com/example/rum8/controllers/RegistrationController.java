e2f3352ef4f8813c904fd410f5398f780a45a5ea
Chase Sriprajittichai <sriprajittichai@gmail.com>
2019-05-29 18:30:58 -0700

    Refactor RegistrationController

--- app/src/main/java/com/example/rum8/controllers/RegistrationController.java
+++ app/src/main/java/com/example/rum8/controllers/RegistrationController.java
@@ -41,14 +41,14 @@ public class RegistrationController {
         return email != null && email.length() >= minimumEmailLength && email.endsWith("@ucsd.edu");
     }
 
-    // check the passwords match
-    private static boolean passWordMatch(final String password, final String passwordConfirm) {
-        return password.equals(passwordConfirm);
-    }
-
     private static boolean isValidPassword(final String password) {
         final int minimumPasswordLength = 6;
         return password != null && password.length() >= minimumPasswordLength;
+    }
+
+    // check the passwords match
+    private static boolean passwordsMatch(final String password, final String passwordConfirm) {
+        return password.equals(passwordConfirm);
     }
 
     public void onSubmit(final String email, final String password, final String passwordConfirm) {
@@ -58,30 +58,28 @@ public class RegistrationController {
         } else if (!isValidPassword(password)) {
             final String message = "Your password need to be more than 6 characters";
             controllerListener.showToast(message);
-        } else if (!passWordMatch(password, passwordConfirm)) {
-            final String message = "Passwords didn't match";
+        } else if (!passwordsMatch(password, passwordConfirm)) {
+            final String message = "Passwords don't match";
             controllerListener.showToast(message);
         } else {
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener
                     ((Activity) context, task -> {
                         if (task.isSuccessful()) {
                             sendVerificationEmail(email);
+
                             controllerListener.goToLogin();
-                            // Create a new user with email when registration is complete
 
                             final Map<String, Object> userInfo = new HashMap<String, Object>() {{
                                 put(Db.Keys.EMAIL, email);
                             }};
-
-                            final FirebaseUser user = auth.getCurrentUser();
 
                             final Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), R.drawable.images);
                             final ByteArrayOutputStream baos = new ByteArrayOutputStream();
                             bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
                             final byte[] data = baos.toByteArray();
 
+                            final FirebaseUser user = auth.getCurrentUser();
                             Db.uploadDefaultPicture(storage, user, data);
-
                             Db.createUser(db, user, userInfo)
                                     .addOnSuccessListener(aVoid -> Log.d("Success", "createUserWithEmail:success"))
                                     .addOnFailureListener(e -> Log.d("Error", "createUserWithEmail:failure", e));
@@ -99,26 +97,18 @@ public class RegistrationController {
         }
     }
 
-    /*
-     * Helper function
-     */
     private void sendVerificationEmail(final String email) {
         auth.getCurrentUser().sendEmailVerification()
                 .addOnCompleteListener(task -> {
                     final String message;
                     if (task.isSuccessful()) {
                         message = "Verification email sent to " + email;
-                        controllerListener.showToast(message);
                     } else {
                         Log.e(TAG, "sendEmailVerification", task.getException());
                         message = "Failed to send verification email to";
-                        controllerListener.showToast(message);
                     }
+                    controllerListener.showToast(message);
                 });
-    }
-
-    public void onGoBackToLoginButtonClicked() {
-        controllerListener.goToLogin();
     }
 
 }
