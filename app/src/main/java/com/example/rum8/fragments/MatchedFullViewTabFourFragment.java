5c89faf2cbd5415c9e9a0fe6ee081a40a8facf4f
Ethan Yuan <etyuan@ucsd.edu>
2019-06-01 08:47:42 -0700

    Finished implementing contact page

--- app/src/main/java/com/example/rum8/fragments/MatchedFullViewTabFourFragment.java
+++ app/src/main/java/com/example/rum8/fragments/MatchedFullViewTabFourFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,35 +18,57 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.rum8.R;
+import com.example.rum8.activities.MatchedRoommateProfileActivity;
+import com.example.rum8.controllers.MatchedRoommateProfileController;
+import com.example.rum8.database.Db;
 import com.example.rum8.listeners.MatchedRoommateProfileControllerListener;
 
 import java.util.Map;
 
 public class MatchedFullViewTabFourFragment extends Fragment implements MatchedRoommateProfileControllerListener {
     View view;
-    private Button emailButton;
+    private MatchedRoommateProfileController controller;
 
     private LinearLayout facebookLinearLayout;
+    private LinearLayout phoneNumberLinearLayout;
+    private LinearLayout snapchatLinearLayout;
+    private LinearLayout emailLinearLayout;
     private TextView facebookTextView;
+    private TextView phoneNumberTextView;
+    private TextView emailTextView;
+    private TextView snapchatTextView;
+    private String emailPassed;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_full_view_tab_four, container, false);
-        emailButton = view.findViewById(R.id.contact_email_button);
 
-        emailButton.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_SENDTO);
-            intent.setData(Uri.parse("mailto:"));
-            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"tempUser@gmail.com"});
-            startActivity(intent);
-        });
+
 
         facebookLinearLayout = view.findViewById(R.id.facebook_linear_layout);
-        facebookTextView = view.findViewById(R.id.text_view_phone_number);
-        facebookLinearLayout.setVisibility(View.GONE);
+        phoneNumberLinearLayout = view.findViewById(R.id.phone_number_linear_layout);
+        phoneNumberTextView = view.findViewById(R.id.phone_number_text_view);
+        facebookTextView = view.findViewById(R.id.facebook_text_view);
+        emailTextView = view.findViewById(R.id.email_text_view);
+        snapchatLinearLayout = view.findViewById(R.id.snapchat_linear_layout);
+        snapchatTextView = view.findViewById(R.id.snapchat_text_view);
+        emailLinearLayout = view.findViewById(R.id.email_linear_layout);
+
+        controller = new MatchedRoommateProfileController(this);
+        controller.loadMatchUserContactInfo(((MatchedRoommateProfileActivity) getActivity()).getMatchedUserId());
+
+        emailLinearLayout.setClickable(true);
 
 
+
+        emailLinearLayout.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:"));
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{emailPassed});
+            startActivity(intent);
+        });
 
 
         return view;
@@ -53,11 +76,45 @@ public class MatchedFullViewTabFourFragment extends Fragment implements MatchedR
 
     @Override
     public void showToast(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     public void showMatchedInfo(Map<String, Object> data) {
+        final String facebook = (String) data.get(Db.Keys.FACEBOOK);
+        final String email = (String) data.get(Db.Keys.EMAIL);
+        final String phoneNumber = (String) data.get(Db.Keys.PHONE_NUMBER);
+        final String snapchat = (String) data.get(Db.Keys.SNAPCHAT);
+
+        emailPassed = email;
+
+        emailTextView.setText(email);
+
+
+        if (facebook.equals("") && facebook.length() <= 0){
+            facebookLinearLayout.setVisibility(View.GONE);
+        }
+        else{
+            facebookTextView.setText(facebook);
+        }
+
+        if (phoneNumber.equals("") && phoneNumber.length() <= 0){
+            phoneNumberLinearLayout.setVisibility(View.GONE);
+        }
+        else{
+            phoneNumberTextView.setText(phoneNumber);
+        }
+
+        if (snapchat.equals("") && snapchat.length() <= 0){
+            snapchatLinearLayout.setVisibility(View.GONE);
+        }
+        else{
+            snapchatTextView.setText(facebook);
+        }
+
+
+        onResume();
 
     }
 
