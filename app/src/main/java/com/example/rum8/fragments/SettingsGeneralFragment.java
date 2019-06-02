a2c9856a27ad658a1edba0e50b0c5f4f09102428
Chase Sriprajittichai <sriprajittichai@gmail.com>
2019-06-02 02:22:13 -0700

    Refactor SettingsGeneralFragment

--- app/src/main/java/com/example/rum8/fragments/SettingsGeneralFragment.java
+++ app/src/main/java/com/example/rum8/fragments/SettingsGeneralFragment.java
@@ -32,6 +32,8 @@ public class SettingsGeneralFragment extends Fragment implements SettingsControl
 
     private final static int MAX_SIZE = 180; // height of imageView
     private final static String PROGRESS_TITLE = "Uploading...";
+
+    private SettingsController controller;
     private TextInputEditText firstNameField;
     private TextInputEditText lastNameField;
     private Spinner genderSpinner;
@@ -43,101 +45,22 @@ public class SettingsGeneralFragment extends Fragment implements SettingsControl
     private Button buttonSave;
     private Button buttonChoosePic;
     private Button buttonUploadPic;
-    private SettingsController controller;
     private ImageView imageView;
     private ProgressDialog progressDialog;
-
-    private static Bitmap resize(final Bitmap image, final int maxWidth, final int maxHeight) {
-        if (maxHeight > 0 && maxWidth > 0) {
-            int width = image.getWidth();
-            int height = image.getHeight();
-            float ratioBitmap = (float) width / (float) height;
-            float ratioMax = (float) maxWidth / (float) maxHeight;
-
-            int finalWidth = maxWidth;
-            int finalHeight = maxHeight;
-            if (ratioMax > ratioBitmap) {
-                finalWidth = (int) ((float) maxHeight * ratioBitmap);
-            } else {
-                finalHeight = (int) ((float) maxWidth / ratioBitmap);
-            }
-            return Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
-        } else {
-            return image;
-        }
-    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        final View rootView = inflater.inflate(R.layout.fragment_settings_general, container, false);
-        return rootView;
+        return inflater.inflate(R.layout.fragment_settings_general, container, false);
     }
 
     @Override
-    public void onViewCreated(View rootView, Bundle savedInstanceState) {
-        controller = new SettingsController(this);
-
-        //NAME FIELDS
-        firstNameField = rootView.findViewById(R.id.general_info_first_name_field);
-        lastNameField = rootView.findViewById(R.id.general_info_last_name_field);
-        controller.usernameEntered = firstNameField.getText().toString();
-        controller.usernameEntered_lastName = lastNameField.getText().toString();
-
-        //FILLING THE GENDER SPINNER
-        genderSpinner = rootView.findViewById(R.id.general_info_gender_spinner);
-        genderAdapter = ArrayAdapter.createFromResource(getActivity(),
-            R.array.settings_gender_items, android.R.layout.simple_spinner_item);
-        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        genderSpinner.setAdapter(genderAdapter);
-
-        //FILLING THE ACADEMIC YEAR SPINNER
-        academicYearSpinner = rootView.findViewById(R.id.general_info_academic_year_spinner);
-        academicYearAdapter = ArrayAdapter.createFromResource(getActivity(),
-            R.array.settings_academic_year_items, android.R.layout.simple_spinner_item);
-        academicYearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        academicYearSpinner.setAdapter(academicYearAdapter);
-
-        //FILLING THE COLLEGE SPINNER
-        collegeSpinner = rootView.findViewById(R.id.general_info_college_spinner);
-        collegeAdapter = ArrayAdapter.createFromResource(getActivity(),
-            R.array.settings_college_items, android.R.layout.simple_spinner_item);
-        collegeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        collegeSpinner.setAdapter(collegeAdapter);
-
+    public void onViewCreated(final View rootView, final Bundle savedInstanceState) {
+        initViews(rootView);
+        initController();
         controller.loadUserInfo();
-
-        progressDialog = new ProgressDialog(getActivity());
-        imageView = rootView.findViewById(R.id.general_info_profile_image_view);
-
-        // fetch user's profile picture
         controller.loadUserProfileImage();
-
-        buttonChoosePic = rootView.findViewById(R.id.general_info_profile_image_upload_button);
-        buttonUploadPic = rootView.findViewById(R.id.general_info_profile_image_save_button);
-
-        buttonSave = rootView.findViewById(R.id.general_info_profile_save_button);
-
-        buttonSave.setOnClickListener(v -> {
-            final Map<String, Object> userInfo = new HashMap<String, Object>() {{
-                put(Db.Keys.FIRST_NAME, firstNameField.getText().toString());
-                put(Db.Keys.LAST_NAME, lastNameField.getText().toString());
-                put(Db.Keys.GENDER, genderSpinner.getSelectedItem().toString());
-                put(Db.Keys.ACADEMIC_YEAR, academicYearSpinner.getSelectedItem().toString());
-                put(Db.Keys.COLLEGE, collegeSpinner.getSelectedItem().toString());
-            }};
-            controller.onSaveButtonClicked(userInfo);
-        });
-
-        buttonChoosePic.setOnClickListener(v -> controller.onChooseImageCliked());
-
-        buttonUploadPic.setOnClickListener(v -> controller.onUploadImageClicked(getFilePath()));
-
-    }
-
-    private Uri getFilePath() {
-        return ((SettingsActivity) getActivity()).getFilePath();
     }
 
     @Override
@@ -149,23 +72,9 @@ public class SettingsGeneralFragment extends Fragment implements SettingsControl
         imageView = view.findViewById(R.id.general_info_profile_image_view);
         Bitmap bitmap = ((SettingsActivity) getActivity()).getBitmap();
         if (bitmap != null) {
-            bitmap = resize(bitmap, MAX_SIZE, MAX_SIZE);
+            bitmap = resizeBitmap(bitmap, MAX_SIZE, MAX_SIZE);
             imageView.setImageBitmap(bitmap);
         }
-    }
-
-    @Override
-    public void chooseImage() {
-        final Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
-        onResume();
-    }
-
-    @Override
-    public void setUserProfileImage(Bitmap bitmap) {
-        imageView.setImageBitmap(bitmap);
     }
 
     @Override
@@ -182,6 +91,45 @@ public class SettingsGeneralFragment extends Fragment implements SettingsControl
 
         firstNameField.setText(userFirstName);
         lastNameField.setText(userLastName);
+    }
+
+    private static Bitmap resizeBitmap(final Bitmap image, final int maxWidth, final int maxHeight) {
+        if (maxWidth <= 0 || maxHeight <= 0) {
+            return image;
+        }
+
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        final float ratioBitmap = (float) width / (float) height;
+        final float ratioMax = (float) maxWidth / (float) maxHeight;
+
+        int finalWidth = maxWidth;
+        int finalHeight = maxHeight;
+        if (ratioMax > ratioBitmap) {
+            finalWidth = (int) ((float) maxHeight * ratioBitmap);
+        } else {
+            finalHeight = (int) ((float) maxWidth / ratioBitmap);
+        }
+
+        return Bitmap.createScaledBitmap(image, finalWidth, finalHeight, true);
+    }
+
+    private Uri getFilePath() {
+        return ((SettingsActivity) getActivity()).getFilePath();
+    }
+
+    @Override
+    public void chooseImage() {
+        final Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+        onResume();
+    }
+
+    @Override
+    public void setUserProfileImage(Bitmap bitmap) {
+        imageView.setImageBitmap(bitmap);
     }
 
     @Override
@@ -204,6 +152,57 @@ public class SettingsGeneralFragment extends Fragment implements SettingsControl
     public void updateUploadImagePercentage(double percentage) {
         final String message = "Uploaded " + (int) percentage + "%";
         progressDialog.setMessage(message);
+    }
+
+    private void initViews(final View rootView) {
+        //NAME FIELDS
+        firstNameField = rootView.findViewById(R.id.general_info_first_name_field);
+        lastNameField = rootView.findViewById(R.id.general_info_last_name_field);
+
+        //FILLING THE GENDER SPINNER
+        genderSpinner = rootView.findViewById(R.id.general_info_gender_spinner);
+        genderAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.settings_gender_items, android.R.layout.simple_spinner_item);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderAdapter);
+
+        //FILLING THE ACADEMIC YEAR SPINNER
+        academicYearSpinner = rootView.findViewById(R.id.general_info_academic_year_spinner);
+        academicYearAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.settings_academic_year_items, android.R.layout.simple_spinner_item);
+        academicYearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        academicYearSpinner.setAdapter(academicYearAdapter);
+
+        //FILLING THE COLLEGE SPINNER
+        collegeSpinner = rootView.findViewById(R.id.general_info_college_spinner);
+        collegeAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.settings_college_items, android.R.layout.simple_spinner_item);
+        collegeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        collegeSpinner.setAdapter(collegeAdapter);
+
+        progressDialog = new ProgressDialog(getActivity());
+        imageView = rootView.findViewById(R.id.general_info_profile_image_view);
+
+        buttonChoosePic = rootView.findViewById(R.id.general_info_profile_image_upload_button);
+        buttonChoosePic.setOnClickListener(v -> controller.onChooseImageCliked());
+
+        buttonUploadPic = rootView.findViewById(R.id.general_info_profile_image_save_button);
+        buttonUploadPic.setOnClickListener(v -> controller.onUploadImageClicked(getFilePath()));
+
+        buttonSave = rootView.findViewById(R.id.general_info_profile_save_button);
+        buttonSave.setOnClickListener(v -> {
+            final Map<String, Object> userInfo = new HashMap<String, Object>() {{
+                put(Db.Keys.FIRST_NAME, firstNameField.getText().toString());
+                put(Db.Keys.LAST_NAME, lastNameField.getText().toString());
+                put(Db.Keys.GENDER, genderSpinner.getSelectedItem().toString());
+                put(Db.Keys.ACADEMIC_YEAR, academicYearSpinner.getSelectedItem().toString());
+                put(Db.Keys.COLLEGE, collegeSpinner.getSelectedItem().toString());
+            }};
+            controller.onSaveButtonClicked(userInfo);
+        });
+    }
+
+    private void initController() {
+        controller = new SettingsController(this);
+        controller.usernameEntered = firstNameField.getText().toString();
+        controller.usernameEntered_lastName = lastNameField.getText().toString();
     }
 
     @Override
