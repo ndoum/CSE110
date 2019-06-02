ca929513415131a3658f9c4a2af00b4ba84ddc19
CassieYud <duy003@ucsd.edu>
2019-06-02 14:29:53 -0700

    go to profile fixing

--- app/src/main/java/com/example/rum8/controllers/SettingsController.java
+++ app/src/main/java/com/example/rum8/controllers/SettingsController.java
@@ -36,24 +36,46 @@ public class SettingsController {
     }
 
     public void onSaveButtonClicked(final Map<String, Object> userHash) {
-        Db.updateUser(db, auth.getCurrentUser(), userHash)
-                .addOnSuccessListener(aVoid -> {
-                    Log.d(TAG, "DocumentSnapshot successfully written");
-                    controllerListener.showToast("Saved");
-                })
-                .addOnFailureListener(e -> controllerListener.showToast("Network error"));
+        Db.fetchUserInfo(db, auth.getCurrentUser())
+            .addOnSuccessListener(documentSnapshot -> {
+                final Map<String, Object> data = documentSnapshot.getData();
+                final String firstName = (String) data.get(Db.Keys.FIRST_NAME);
+                final String lastName = (String) data.get(Db.Keys.LAST_NAME);
+
+                // check for valid name
+                if (!isPresent(firstName) || !isPresent(lastName)) {
+                    final String message = "Please enter and save your first and last name";
+                    controllerListener.showToast(message);
+                } else {
+                    Db.updateUser(db, auth.getCurrentUser(), userHash)
+                        .addOnSuccessListener(aVoid -> {
+                            Log.d(TAG, "DocumentSnapshot successfully written");
+                            controllerListener.showToast("Saved");
+                        })
+                        .addOnFailureListener(e -> controllerListener.showToast("Network error"));
+                }
+            })
+            .addOnFailureListener(e -> {
+                final String message = "Network error";
+                controllerListener.showToast(message);
+            });
     }
 
     public void loadUserInfo() {
         Db.fetchUserInfo(db, auth.getCurrentUser())
-                .addOnSuccessListener(documentSnapshot -> {
-                    final Map<String, Object> data = documentSnapshot.getData();
-                    controllerListener.showCurrentUserInfo(data);
-                })
-                .addOnFailureListener(e -> {
-                    final String message = "Network error";
-                    controllerListener.showToast(message);
-                });
+            .addOnSuccessListener(documentSnapshot -> {
+                final Map<String, Object> data = documentSnapshot.getData();
+                controllerListener.showCurrentUserInfo(data);
+            })
+            .addOnFailureListener(e -> {
+                final String message = "Network error";
+                controllerListener.showToast(message);
+            });
+    }
+
+    // helper method to check if user input is present
+    private static boolean isPresent(final String name) {
+        return name != null && !name.isEmpty();
     }
 
     public void onChooseImageCliked() {
@@ -66,40 +88,40 @@ public class SettingsController {
             final FirebaseStorage storage = FirebaseStorage.getInstance();
             controllerListener.showUploadImageProgress();
             Db.updateProfilePicture(storage, user, filePath)
-                    .addOnSuccessListener(taskSnapshot -> {
-                        controllerListener.hideUploadImageProgress();
-                        final String message = "Successfully uploaded";
-                        controllerListener.showToast(message);
-                    })
-                    .addOnFailureListener(e -> {
-                        controllerListener.hideUploadImageProgress();
-                        final String message = "Network error";
-                        controllerListener.showToast(message);
-                    })
-                    .addOnProgressListener(taskSnapshot -> {
-                        double progress = (ONE_HUNDRED_PERCENT * taskSnapshot.getBytesTransferred() / taskSnapshot
-                                .getTotalByteCount());
-                        controllerListener.updateUploadImagePercentage(progress);
-                    });
+                .addOnSuccessListener(taskSnapshot -> {
+                    controllerListener.hideUploadImageProgress();
+                    final String message = "Successfully uploaded";
+                    controllerListener.showToast(message);
+                })
+                .addOnFailureListener(e -> {
+                    controllerListener.hideUploadImageProgress();
+                    final String message = "Network error";
+                    controllerListener.showToast(message);
+                })
+                .addOnProgressListener(taskSnapshot -> {
+                    double progress = (ONE_HUNDRED_PERCENT * taskSnapshot.getBytesTransferred() / taskSnapshot
+                        .getTotalByteCount());
+                    controllerListener.updateUploadImagePercentage(progress);
+                });
         }
     }
 
     public void loadUserProfileImage() {
         Db.fetchUserProfilePicture(storage, auth.getCurrentUser())
-                .addOnSuccessListener(bytes -> {
-                    final Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                    controllerListener.setUserProfileImage(bmp);
-                })
-                .addOnFailureListener(e -> {
-                    // fetch default if the user does not upload
-                    controllerListener.showDefaultImage();
-                    // show error message if both way fails
-                    int errorCode = ((StorageException) e).getErrorCode();
-                    if (errorCode != StorageException.ERROR_OBJECT_NOT_FOUND) {
-                        final String message = "Network error";
-                        controllerListener.showToast(message);
-                    }
-                });
+            .addOnSuccessListener(bytes -> {
+                final Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                controllerListener.setUserProfileImage(bmp);
+            })
+            .addOnFailureListener(e -> {
+                // fetch default if the user does not upload
+                controllerListener.showDefaultImage();
+                // show error message if both way fails
+                int errorCode = ((StorageException) e).getErrorCode();
+                if (errorCode != StorageException.ERROR_OBJECT_NOT_FOUND) {
+                    final String message = "Network error";
+                    controllerListener.showToast(message);
+                }
+            });
     }
 
 }
