2a1ada9bfc0e064338759b3adf2fe085055041e5
aiiitingx <aihsieh@ucsd.edu>
2019-06-05 15:43:47 -0700

    get random link uid

--- app/src/main/java/com/example/rum8/activities/RandomLink.java
+++ app/src/main/java/com/example/rum8/activities/RandomLink.java
@@ -1,16 +1,29 @@
 package com.example.rum8.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.rum8.R;
+import com.example.rum8.controllers.RandomLinkController;
+import com.example.rum8.listeners.RandomLinkControllerListener;
 
-public class RandomLink extends AppCompatActivity {
+public class RandomLink extends AppCompatActivity implements RandomLinkControllerListener {
 
+    private RandomLinkController controller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_link);
+        initController();
+        initView();
+    }
+
+    private void initView() {
+        controller.getRandomLink();
+    }
+
+    private void initController() {
+        this.controller = new RandomLinkController(this);
     }
 }
---
+++ app/src/main/java/com/example/rum8/controllers/RandomLinkController.java
@@ -0,0 +1,52 @@
+package com.example.rum8.controllers;
+
+import com.example.rum8.database.Db;
+import com.example.rum8.listeners.RandomLinkControllerListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Random;
+
+public class RandomLinkController {
+    private RandomLinkControllerListener controllerListener;
+    private FirebaseFirestore db;
+    private FirebaseStorage storage;
+    private FirebaseAuth auth;
+
+    public RandomLinkController(final RandomLinkControllerListener controllerListener){
+        this.controllerListener = controllerListener;
+        this.db = FirebaseFirestore.getInstance();
+        this.storage = FirebaseStorage.getInstance();
+        this.auth = FirebaseAuth.getInstance();
+    }
+
+    //randomly choose a link from list
+    public void getRandomLink() {
+        Db.fetchUserInfo(db, auth.getCurrentUser()).addOnCompleteListener((Task<DocumentSnapshot> task) -> {
+            if (task.isSuccessful()) {
+                // fetch current user's matched links
+                HashMap<String, Object> linkListUidMap = (HashMap<String, Object>)task.getResult().get(Db.Keys.MATCHED);
+                int mapSize = linkListUidMap.size();
+                if(mapSize>0){
+                    Random rand = new Random();
+                    int randomNum = rand.nextInt(mapSize-1);
+                    HashMap.Entry<String, Object> randomEntry;
+                    Iterator<HashMap.Entry<String, Object>> mapItr = linkListUidMap.entrySet().iterator();
+                    randomEntry = mapItr.next();
+                    while(mapItr.hasNext() && randomNum >= 0){
+                        randomEntry = mapItr.next();
+                        randomNum--;
+                    }
+                    String randomUid = randomEntry.getKey();
+                    System.out.println("RANDOM UID: "+randomUid);
+                }
+
+            }
+            });
+    }
+}
