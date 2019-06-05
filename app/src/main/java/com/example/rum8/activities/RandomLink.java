66bc1ddc91ed77ade35829ba45c93116fbee6385
CassieYud <duy003@ucsd.edu>
2019-06-05 14:20:46 -0700

    randomLink

---
+++ app/src/main/java/com/example/rum8/activities/RandomLink.java
@@ -0,0 +1,16 @@
+package com.example.rum8.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rum8.R;
+
+public class RandomLink extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_link);
+    }
+}
