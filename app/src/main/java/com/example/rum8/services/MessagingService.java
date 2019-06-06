a4086cbb970a278919d71f462665299c1c3d68da
Chase Sriprajittichai <sriprajittichai@gmail.com>
2019-06-05 19:34:40 -0700

    Change MessagingService to expect FCM data message, not FCM notification message

--- app/src/main/java/com/example/rum8/services/MessagingService.java
+++ app/src/main/java/com/example/rum8/services/MessagingService.java
@@ -6,64 +6,56 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
 import com.example.rum8.R;
-import com.example.rum8.activities.MainActivity;
+import com.example.rum8.activities.ViewLinkListActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 public class MessagingService extends FirebaseMessagingService {
 
-    private static final String TAG = "MyFirebaseMsgService";
-
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-
-        Log.d("Success", "Message recieved from: " + remoteMessage.getFrom());
-
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser user = auth.getCurrentUser();
-
-        if (user == null)
+    public void onMessageReceived(final RemoteMessage remoteMessage) {
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             return;
-
-        if (remoteMessage.getNotification() != null) {
-            Log.d("Success", "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            sendNotification(remoteMessage.getNotification().getBody());
         }
+
+        final Map<String, String> data = remoteMessage.getData();
+        final String title = data.get("title");
+        final String body = data.get("body");
+
+        Log.d("Success", String.format("Message received from: %s", remoteMessage.getFrom()));
+        Log.d("Message title", title);
+        Log.d("Message body", body);
+
+        sendNotification(title, body);
     }
 
-    private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
+    private void sendNotification(final String title, final String body) {
+        final Intent intent = new Intent(this, ViewLinkListActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        String channelId = getString(R.string.default_notification_channel_id);
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.logo_no_margin_small)
-                        // .setContentTitle(getString(R.string.fcm_message))
-                        .setContentText(messageBody)
-                        .setAutoCancel(true)
-                        .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
+        final String channelId = getString(R.string.default_notification_channel_id);
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.logo_no_margin_small)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(pendingIntent);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel(channelId, "default notification channel", NotificationManager.IMPORTANCE_HIGH);
             notificationManager.createNotificationChannel(channel);
         }
 
